4cee2791ebd3393aaa5334a387f3d9e64c1996cf
LanceXu <59581718+bananamilkt@users.noreply.github.com>
2022-06-18 16:20:53 -0700

    INIT

---
+++ main.py
@@ -0,0 +1,161 @@
+import pywt
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import tensorflow_wavelets.Layers.DWT as wavelet
+from keras.preprocessing.image import ImageDataGenerator
+
+TRAIN_DATA_PATH = "Data/train"
+VALID_DATA_PATH = "Data/valid"
+TEST_DATA_PATH  = "Data/test"
+BATCH_SIZE      = 16
+TARGET_SIZE     = (227,227)
+INPUT_SHAPE     = [227,227,3]
+
+train_data_generator = tf.keras.preprocessing.image.ImageDataGenerator( rescale = 1.0/255.0,
+                                                                        horizontal_flip = True,
+                                                                        fill_mode = 'nearest',
+                                                                        zoom_range=0.2,
+                                                                        shear_range = 0.2,
+                                                                        width_shift_range=0.2,
+                                                                        height_shift_range=0.2,
+                                                                        rotation_range=0.4)
+
+train_data = train_data_generator.flow_from_directory(  TRAIN_DATA_PATH,
+                                                        batch_size = BATCH_SIZE,
+                                                        target_size = TARGET_SIZE,
+                                                        class_mode = 'categorical')
+
+validation_data_generator = ImageDataGenerator(rescale = 1.0/255.0)
+
+validation_data = validation_data_generator.flow_from_directory(     VALID_DATA_PATH,
+                                                        target_size = TARGET_SIZE,
+                                                        batch_size = BATCH_SIZE,
+                                                        class_mode = 'categorical')
+class CNNModel:
+    def __init__(self, input_shape=0, model_type='', model_path=None):
+        self.model_type = model_type
+        self.history = None
+        self.history_accuracy = []
+        self.history_val_accuracy = []
+        self.history_loss = []
+        self.history_val_loss = []
+        self.input_shape = input_shape
+        self.model = tf.keras.models.Sequential()
+        if(model_path==None):
+            self.model_path = 'savedModel/CNNModel.ckpt'
+        else:
+            self.model_path = model_path
+        self.model_checkpoint = tf.keras.callbacks.ModelCheckpoint(self.model_path,save_weights_only=True,verbose=1)
+
+    def fit(self,train_data, validation_data, epochs):
+        self.history = self.model.fit(x = train_data, validation_data = validation_data, epochs = epochs, callbacks=[self.model_checkpoint])
+        self.history_accuracy.extend(self.history.history['accuracy'])
+        self.history_val_accuracy.extend(self.history.history['val_accuracy'])
+        self.history_loss.extend(self.history.history['loss'])
+        self.history_val_loss.extend(self.history.history['val_loss'])
+
+    def plotAccuracy(self):
+        if(self.history != None):
+            print(self.history.history.keys())
+            plt.plot(self.history_accuracy)
+            plt.plot(self.history_val_accuracy)
+            plt.title('model accuracy')
+            plt.ylabel('accuracy')
+            plt.xlabel('epoch')
+            plt.legend(['train','test'], loc='upper left')
+            plt.show()
+        
+    def plotLoss(self):
+        if(self.history != None):
+            plt.plot(self.history_loss)
+            plt.plot(self.history_val_loss)
+            plt.title('model loss')
+            plt.ylabel('loss')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'test'], loc='upper left')
+            plt.show()
+
+    def compareAccuracy(models):
+        modelList = []
+        for model in models:
+            if(model.history != None):
+                plt.plot(model.history_accuracy)
+                modelList.append(model.model_type)
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(modelList, loc='upper left')
+        plt.show()
+
+    def compareLoss(models):
+        modelList = []
+        for model in models:
+            if(model.history != None):
+                plt.plot(model.history_loss)
+                modelList.append(model.model_type)
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(modelList, loc='upper left')
+        plt.show()
+
+    def predict(self, path):
+        classes_dir = ["Adenocarcinoma","Large cell carcinoma","Normal","Squamous cell carcinoma"]
+        image = tf.keras.utils.load_img(path, target_size=TARGET_SIZE)
+        normalizedImage = tf.keras.utils.img_to_array(image)/255
+        imageArray = np.array([normalizedImage])
+        predtionResult = np.argmax(self.model.predict(imageArray))
+        print(classes_dir[predtionResult])
+
+    def load(self,model_path=None):
+        if(model_path==None):
+            self.model.load_weights(self.model_path)
+        else:
+            self.model.load_weights(model_path)
+class AlexNet(CNNModel):
+    def __init__(self,input_shape=0,model_path='savedModel/AlexNet.ckpt'):
+
+        super().__init__(input_shape=input_shape,model_type="AlexNet",model_path=model_path)
+        self.model.add(tf.keras.layers.Conv2D(filters=96, kernel_size=11, activation='relu', strides=4, input_shape=input_shape))
+        self.model.add(tf.keras.layers.MaxPool2D(pool_size=3,strides=2))
+        self.model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=5, activation='relu'))
+        self.model.add(tf.keras.layers.MaxPool2D(pool_size=3,strides=2))
+        self.model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=3, activation='relu'))
+        self.model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=3, activation='relu'))
+        self.model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=3, activation='relu'))
+        self.model.add(tf.keras.layers.MaxPool2D(pool_size=3,strides=2))
+        self.model.add(tf.keras.layers.Flatten())
+        self.model.add(tf.keras.layers.Dense(units=4096, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(units=4096, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(units=4, activation='softmax'))
+        self.model.compile(loss='categorical_crossentropy', optimizer="sgd", metrics = ['accuracy'])      
+class WaveletAlexNet(CNNModel):
+    def __init__(self,input_shape=0,model_path='savedModel/WaveletAlexNet.ckpt'):
+        opt = tf.keras.optimizers.SGD(momentum=0.9)
+
+        super().__init__(input_shape=input_shape,model_type="WaveletAlexNet",model_path=model_path)
+        self.model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=5, activation='relu', strides=4, input_shape=input_shape))
+
+        self.model.add(wavelet.DWT(name='bd2'))
+        self.model.add(tf.keras.layers.Reshape((112, 112, 96)))
+
+        self.model.add(tf.keras.layers.MaxPool2D(pool_size=3,strides=2))
+        self.model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=5, activation='relu'))
+        self.model.add(tf.keras.layers.MaxPool2D(pool_size=3,strides=2))
+        self.model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=3, activation='relu'))
+        self.model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=3, activation='relu'))
+        self.model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=3, activation='relu'))
+        self.model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=3, activation='relu'))
+        self.model.add(tf.keras.layers.MaxPool2D(pool_size=3,strides=2))
+        self.model.add(tf.keras.layers.Flatten(input_shape = (2,2,384)))
+        self.model.add(tf.keras.layers.Dense(units=4096, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(units=4096, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(units=4, activation='softmax'))
+        self.model.compile(loss='categorical_crossentropy', optimizer="sgd", metrics = ['accuracy'])
+        
+alexNet = AlexNet(INPUT_SHAPE)
+alexNet.fit(train_data, validation_data, 2)
+
--- readme.md
+++ readme.md
@@ -1,15 +1,38 @@
-# Chest Cancer Detection Project 
+# Lung Cancer Classification CNN
 
-## Source of data
-Chest CT-Scan images Dataset provided by Mohamed Hany Data Scientist at QN Academy.
-##### Dataset Page
-https://www.kaggle.com/datasets/mohamedhanyyy/chest-ctscan-images
-##### Author Page
-https://www.kaggle.com/mohamedhanyyy
+### Source of data
+##### Dataset
+- https://www.kaggle.com/datasets/mohamedhanyyy/chest-ctscan-images
+    ###### Author Page https://www.kaggle.com/mohamedhanyyy
+    ###### Chest CT-Scan images Dataset provided by Mohamed Hany Data Scientist at QN Academy.
 
-## Credits of implementation
-CNN implementation for Chest Cancer Detection by Likitha Vadlapudi student at vignan's institute of information and technology.
-##### Implementation Page
-https://www.kaggle.com/code/likithavadlapudi/notebook095d438171
-##### Author Page
-https://www.kaggle.com/likithavadlapudi
+### Credits of implementation
+##### Referencing Code
+- https://www.kaggle.com/code/likithavadlapudi/notebook095d438171
+    ###### Author Page https://www.kaggle.com/likithavadlapudi 
+    ###### CNN implementation for Chest Cancer Detection by Likitha Vadlapudi student at vignan's institute of information and technology.
+- https://keras.io/api/data_loading/image/
+- https://www.tensorflow.org/tutorials/images/cnn
+
+### Referencing paper
+- Guangyu Jia, Hak-Keung Lam, Yujia Xu,
+  Classification of COVID-19 chest X-Ray and CT images using a type of dynamic CNN modification method,
+  Computers in Biology and Medicine,
+  Volume 134,
+  2021,
+  104425,
+  ISSN 0010-4825,
+  https://doi.org/10.1016/j.compbiomed.2021.104425.
+  (https://www.sciencedirect.com/science/article/pii/S0010482521002195)
+  
+- Dipanjan Moitra, Rakesh Kr. Mandal,
+  Classification of non-small cell lung cancer using one-dimensional convolutional neural network,
+  Expert Systems with Applications,
+  Volume 159,
+  2020,
+  113564,
+  ISSN 0957-4174,
+  https://doi.org/10.1016/j.eswa.2020.113564.
+  (https://www.sciencedirect.com/science/article/pii/S0957417420303882)
+
+
